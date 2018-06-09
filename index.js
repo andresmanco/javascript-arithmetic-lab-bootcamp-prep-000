820b0c8509b0e6fd85ef47c3277d28891a17c8fe
Andres Manco <andresmanco@gmail.com>
2018-06-09 08:41:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ function add(a, b) {
   return a + b
 }
 
+function substract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return a + b
+}
